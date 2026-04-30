d649bc08b6c68aee6fd4b92ea64094326c6ce7fd
Bruno Braga <bf.braga@campus.fct.unl.pt>
2026-04-30 18:11:28 +0100

    refactor: extract interfaces to dedicated file

--- server/internal/server/hub.go
+++ server/internal/server/hub.go
@@ -5,57 +5,23 @@ import (
 	"net/http"
 
 	"github.com/bfibraga/turntide/server/internal/objects"
-	"github.com/bfibraga/turntide/server/internal/server/repository"
+	"github.com/bfibraga/turntide/server/internal/server/user"
 	"github.com/bfibraga/turntide/server/pkg/packets"
 )
 
-type ClientInterfacer interface {
-	// Id returns the client's unique identifier.
-	Id() uint64
-	// ProcessPacket processes a packet from the hub.
-	ProcessPacket(senderId uint64, message packets.Msg)
-
-	Initialize(id uint64)
-	SocketSend(message packets.Msg)
-	SocketSendAs(senderId uint64, message packets.Msg)
-
-	PassToPeer(message packets.Msg, peerId uint64)
-	Broadcast(message packets.Msg)
-	ReadPump()
-	WritePump()
-
-	SetState(state ClientStateHandler)
-
-	Close()
-}
-
-// A structure for a state machine to process the client's messages
-type ClientStateHandler interface {
-	Name() string
-
-	// Inject the client into the state handler
-	SetClient(client ClientInterfacer)
-
-	OnEnter()
-	HandleMessage(senderId uint64, message packets.Msg)
-
-	// Cleanup the state handler and perform any last actions
-	OnExit()
-}
-
 type Hub struct {
 	Logger         *slog.Logger
-	UserRepository repository.UserRepository
+	UserService    *user.Service
 	Clients        *objects.SharedCollection[ClientInterfacer]
 	BroadcastChan  chan *packets.Packet
 	RegisterChan   chan ClientInterfacer
 	UnregisterChan chan ClientInterfacer
 }
 
-func NewHub(logger *slog.Logger, userRepo repository.UserRepository) *Hub {
+func NewHub(logger *slog.Logger, userService *user.Service) *Hub {
 	return &Hub{
 		Logger:         logger,
-		UserRepository: userRepo,
+		UserService:    userService,
 		Clients:        objects.NewSharedCollection[ClientInterfacer](),
 		BroadcastChan:  make(chan *packets.Packet),
 		RegisterChan:   make(chan ClientInterfacer),
@@ -65,7 +31,7 @@ func NewHub(logger *slog.Logger, userRepo repository.UserRepository) *Hub {
 
 func (h *Hub) Run() {
 	h.Logger.Info("Initializing database")
-	
+
 	h.Logger.Info("Waiting for connections...")
 	for {
 		select {
